b172c19066682e7c0bfb2132215eab722d7286b4
Hayat6488 <abulhayat619@gmail.com>
2022-11-09 04:24:10 +0600

    find with specific id added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -12,18 +12,38 @@ app.use(express.json());
 
 //mongodb conncetion
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.djdi1bf.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         const foodsCollection = client.db('dhekur').collection('foods');
 
+        app.get('/food', async (req, res) => {
+            const query = {};
+            const cursor = foodsCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+
         app.get('/foods', async (req, res) => {
             const query = {};
             const cursor = foodsCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        });
+
+        // app.get('/services/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const service = await serviceCollection.findOne(query);
+        //     res.send(service);
+        // });
+
+        app.get('/foods/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const food = await foodsCollection.findOne(query);
+            res.send(food);
         })
     }
     finally {
