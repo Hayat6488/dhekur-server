540da091b4841b3b9b9d11a8c9313b085cda20a2
Hayat6488 <abulhayat619@gmail.com>
2022-11-08 19:29:26 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+//mongodb conncetion
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.djdi1bf.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', (req, res) => {
     res.send('Dhekur server running');
